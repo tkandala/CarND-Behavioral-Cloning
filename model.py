458321067bdb480dc26eb5fdaf097d478f7b9a12
kingtarun <tkandala@gmail.com>
2017-02-01 23:40:19 -0500

    finally fixed the data shape issue

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ dataframe = pandas.read_csv(training_data_file, usecols=['center', 'steering'])
 
 X = dataframe.values[:,0]
 y = dataframe.values[:,1]
+Y = y.reshape((1,) + y.shape)
+y = Y[0]
 
 from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasRegressor
@@ -26,13 +28,15 @@ for i in range(len(X)):
 	image = load_img(X[i])
 	image_PIL = Image.open(X[i])
 	image_array = np.asarray(image_PIL)
-	image_array = image_array[None, :, :, :]
 	#print(image_array.shape)
 	#x = img_to_array(image)
 	#x = x.reshape((1,) + x.shape)
 	x.append(image_array)
 
-X = x
+x = np.asarray(x)
+X = x[None, :, :, :]
+X = X[0]
+#X = np.delete(X, 0, 1)
 
 #from sklearn.utils import shuffle
 #X,y = shuffle(X,y)
@@ -43,18 +47,22 @@ def steering_model():
 	# create model
 	model = Sequential()
 	model.add(Lambda(lambda x: x/127.5 - 1.,input_shape=(160, 320, 3),output_shape=(160, 320, 3)))
-	#model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
-	#model.add(Convolution2D(32,3,3,border_mode='valid',input_shape=(320,160,3)))
-	#model.add(ELU())
-	#model.add(Convolution2D(80,3,3,border_mode='valid'))
+	model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+	model.add(ELU())
+	model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+	model.add(ELU())
+	model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
 	#model.add(ELU())
 	#model.add(Convolution2D(80,3,3,border_mode='valid'))
 	#model.add(MaxPooling2D(pool_size=(2,2)))
 	model.add(Flatten())
 	model.add(Dropout(0.2))
-	model.add(Activation('relu'))
+	#model.add(Activation('relu'))
+	model.add(ELU())
 	#model.add(Dense(1, init='normal', activation='relu'))
-	model.add(Dense(1, init='normal'))
+	model.add(Dense(512))
+	model.add(ELU())
+	model.add(Dense(1))
 	# Compile model
 	model.compile(loss='mean_squared_error', optimizer='adam')
 	return model
